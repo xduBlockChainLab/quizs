7c5895c0e0cd87b68e21e0fa6ff141d6265758f0
zhangxiaorui <zxr35l@163.com>
2022-04-08 11:54:28 +0800

    Add initial structure.

---
+++ quiz_main/src/main/java/io/stars/quiz_main/QuizMainApplication.java
@@ -0,0 +1,13 @@
+package io.stars.quiz_main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizMainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuizMainApplication.class, args);
+    }
+
+}
---
+++ quiz_main/src/test/java/io/stars/quiz_main/QuizMainApplicationTests.java
@@ -0,0 +1,13 @@
+package io.stars.quiz_main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizMainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
